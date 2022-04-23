6eb394e4bd496417d4ca926e58c6db5e07f3d046
handal <handal__@naver.com>
2022-04-23 17:58:32 +0900

    REFACTOR: setup code 분할

--- cmd/main.go
+++ cmd/main.go
@@ -8,15 +8,21 @@ import (
 )
 
 func main() {
-	router := gin.Default()
+	server := setupServer()
 
-	loaders.LoadAPIs(router)
-
-	err := router.Run(":3000")
+	err := server.Run(":3000")
 	if err != nil {
 		fmt.Printf("Fail to start Server")
 		return
 	}
 
 	fmt.Printf("Server Run")
+}
+
+func setupServer() *gin.Engine {
+	router := gin.Default()
+
+	loaders.LoadAPIs(router)
+
+	return router
 }
